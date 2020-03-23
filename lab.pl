6b8b433fb3fd0e3d4c706ef98b8948a5d5b9e6f0
m-cyx <markpixel05@gmail.com>
2020-03-23 16:24:40 +0300

    дополнил вторую и третью ПЗ
    расставил номера, дописал некоторые задачи

--- lab.pl
+++ lab.pl
@@ -1,42 +1,6 @@
 %=========================================================================
-                            % Простые предикаты
+                            % Пзшки и домашки 
 %=========================================================================
-        % Факториал числа 
-fact(0,1):-!.
-fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
-fact1(N,X):-fact2(0,1,N,X).
-fact2(N,K,N,K):-!.
-fact2(I,K,N,X):-I1 is I+1,K1 is K*I1,fact2(I1,K1,N,X).
-
-        % Число Фибоначи заданного номера 
-fibo(1,1):-!.
-fibo(2,1):-!.
-fibo(N,X):-N1 is N-1,fibo(N1,X1),N2 is N-2,fibo(N2,X2),X is X1+X2.
-    
-fibo1(N,X):-fibo2(1,1,2,N,X).
-fibo2(_,K,N,N,K):-!.
-fibo2(J,K,I,N,X):-I1 is I+1, K1 is J+K, fibo2(K,K1,I1,N,X).
-
-        % Максимум из двух и трёх
-max(X,Y,X):-X>Y,!.
-max(_,Y,Y).
-
-max3(X,Y,U,Z):-max(Y,U,K),max(X,K,Z).
-
-        % Простота и Наибольший простой делитель
-pr(2):-!.
-pr(X):-pr1(2,X).
-pr1(X,X):-!.
-pr1(I,X):-Y is X mod I, not(Y=0), I1 is I+1, pr1(I1,X).
-    
-npr_d(N,X):-npd(N,N,X).
-npd(I,N,X):-Y is N mod I,Y=0,pr(I).
-npd(I,N,X):-I1 is I-1,ndp(I1,N,X).
-    
-        %сумма цифр числа
-sum(X,X):- X<10, !.
-sum(X,S):-L is X mod 10, B is X div 10, sum(B,S1), S is S1+L.
-
 
 % Lab_1 tree
 %========================================================================
@@ -78,8 +42,50 @@ is_br_si(X,Y):-sister(X,Y).
 uncle(X,Y):-man(X),child(Y,M),is_br_si(Z,M),married(X,Z).
 aunt(X,Y):-woman(X),child(Y,M),is_br_si(Z,M),married(X,Z).
 
+% Lab_2 Алгоритмы работы с числами 
+%========================================================================
 
-% Lab_3 Lists (списки)
+        % 2.1 Максимум из двух чисел.
+max(X,Y,X):-X>Y,!.
+max(_,Y,Y).
+        % 2.2 Максимум из трёх чисел.
+max3(X,Y,U,Z):-max(Y,U,K),max(X,K,Z).
+
+        % 2.3 Факториал числа 
+        fact(0,1):-!.
+fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
+fact1(N,X):-fact2(0,1,N,X).
+fact2(N,K,N,K):-!.
+fact2(I,K,N,X):-I1 is I+1,K1 is K*I1,fact2(I1,K1,N,X).
+
+        % 2.4 Число Фибоначи заданного номера.
+fibo(1,1):-!.
+fibo(2,1):-!.
+fibo(N,X):-N1 is N-1,fibo(N1,X1),N2 is N-2,fibo(N2,X2),X is X1+X2.
+    
+fibo1(N,X):-fibo2(1,1,2,N,X).
+fibo2(_,K,N,N,K):-!.
+fibo2(J,K,I,N,X):-I1 is I+1, K1 is J+K, fibo2(K,K1,I1,N,X).
+
+        % 2.5 Является число простым?
+pr(2):-!.
+pr(X):-pr1(2,X).
+pr1(X,X):-!.
+pr1(I,X):-Y is X mod I, not(Y=0), I1 is I+1, pr1(I1,X).
+    
+        % 2.7 Сумма цифр числа
+sumcifr(X,X):- X<10, !.
+sumcifr(X,S):-L is X mod 10, B is X div 10, sumcifr(B,S1), S is S1+L.
+
+        % 2.8 Найти наибольший простой делитель числа N.
+npr_d(N,X):-npd(N,N,X).
+npd(I,N,X):-Y is N mod I,Y=0,pr(I).
+npd(I,N,X):-I1 is I-1,ndp(I1,N,X).
+
+        % 2.9 НОД Двух чисел
+nod(X,Y,Y):-R is X mod Y,(R = 0),!.
+nod(X,Y,D):-R is X mod Y,nod(Y,R,D).
+% Lab_3 Списки
 %========================================================================
 
             % вводим и выводим список, функции ниже
@@ -94,26 +100,38 @@ r_list(A,N,I,B):-I1 is I+1, read(X), append(B,[X],B1),r_list(A,N,I1,B1).
 w_list([]):-!.
 w_list([H|T]):-write(H),nl,w_list(T).
 
-        % найти сумму элементов списка 
-pr3_1:-read(N), r_list(A,N), sum_list(A,Z), write(Z).
-%Читаем число, вводим список длины N, sumlist, вывод ответа.
+        % 3.1 Найти сумму элементов списка 
+pr3_1:-
+        read(N), 
+        r_list(A,N), 
+        sum_list(A,Z), 
+        write(Z).
 sum_list(A,Z):-sum_list(A,Z,0).
 sum_list([],Z,Z):-!.
 sum_list([H|T],Z,S):- S1 is S+H, sum_list(T,Z,S1).
 
-         % предикат, получающий элемент списка с заданным номером
-pr3_2:-read(N), r_list(A,N), read(I), el_no(A,I,Z), write(Z).
+         % 3.2 предикат, получающий элемент списка с заданным номером
+pr3_2:-
+        read(N), 
+        r_list(A,N), 
+        read(I), 
+        el_no(A,I,Z), 
+        write(Z).
 el_no(A,I,Z):-el_no(A,I,1,Z).
 el_no([H|_],I,I,H):-!.
 el_no([_|T],I,K,Z):- K1 is K+1, el_no(T,I,K1,Z).
 
-        % Найти минимальный элемент в списке 
-pr3_3:-read(N), r_list(A,N), min(A,M), write(M).
+        % 3.3 Найти минимальный элемент в списке 
+pr3_3:-
+        read(N), 
+        r_list(A,N), 
+        min(A,M), 
+        write(M).
 min([H|T],M):-min(T,M,H).
 min([],M,M):-!.
 min([H|T],M,Mi):-H<Mi, !, min(T,M,H).
 min([_|T],M,Mi):-min(T,M,Mi).
-        % предикат, который возвращает true, если элемент есть в списке
+        % 3.4 Предикат, который возвращает true, если элемент есть в списке
 pr3_4:- 
         write('size of list: '), 
         read(N), 
@@ -131,7 +149,7 @@ pr_list([Head|Tail],Pr,P):- P1 is P*Head, pr_list(Tail,Pr,P1).
 
 % Lab_5 Чтение и тд.
 %========================================================================
-        % Word \=[]           ----- слово не пустое 
+        % Word \=[]  -> слово не пустое 
         % прочитать строку 
 read_str(A,N):-get0(X), r_str(X,A,[],N,0).
 r_str(10,A,A,N,N):-!.
@@ -161,11 +179,13 @@ pr5_1:-
 
 % Дана строка. Найти количество слов. 
 %(вводим через пробел и нажимаем энтер )
+
 pr5_2:-read_str(A,N),count_words(A,K),write(K).
 
 count_words(A,K):-count_words(A,0,K).
 count_words([],K,K):-!.
-count_words(A,I,K):-skip_space(A,A1),get_word(A1,Word,A2),Word \=[],I1 is I+1,count_words(A2,I1,K),!.
+count_words(A,I,K):-skip_space(A,A1),get_word(A1,Word,A2),
+                        Word \=[],I1 is I+1,count_words(A2,I1,K),!.
 count_words(_,K,K).
 
 skip_space([32|T],A1):-skip_space(T,A1),!.
