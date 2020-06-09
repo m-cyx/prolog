6ae563cb5902c2199df1dfe6f6bfe4e24faeb3dc
m-cyx <markpixel05@gmai.com>
2020-06-09 23:48:05 +0300

    работает

--- ind_11.fs
+++ ind_11.fs
@@ -1,21 +1,43 @@
 open System
 open System.IO
-open System.Windows.Forms
 open System.Drawing
+open System.Windows.Forms
 
-let form = new Form(Width= 400, Height = 300, Text = "Высокотехнологичное программное обеспечение")
-let cb1 = new CheckBox(Text="1", Top=20, Left=50)
-let cb2 = new CheckBox(Text="2", Top=20, Left=200)
-let textbox = new TextBox(Left=50, Top=100, Width=300, Text="")
-let button = new Button(Text="чек", Left=50, Top=150, Height = 50,  Width=300)
 
-form.Controls.Add(cb1)
-form.Controls.Add(cb2)
-form.Controls.Add(textbox)
-form.Controls.Add(button)
+
+
+let window = new Form(Width= 400, Height = 300, Text = "Высокотехнологичное программное обеспечение")
+let cb1 = new CheckBox(Text="Первый", Top=20, Left=50)
+let cb2 = new CheckBox(Text="Второй", Top=20, Left=200)
+let textbox = new TextBox(Text="Внимание: не нажимайте на кнопку ниже!", Left=50, Top=50, Width=300)
+let button = new Button(Text="НЕ НАЖИМАТЬ!", Left=50, Top=100, Width=300, Height = 50)
+window.Controls.Add(cb1)
+window.Controls.Add(cb2)
+window.Controls.Add(textbox)
+window.Controls.Add(button)
+
+
+
+//если нажата кнопка 
+let knopka_press _ = 
+    if (cb1.Checked=false && cb2.Checked=false) then
+        textbox.Text<-"Галочки не установлены"
+    if (cb1.Checked=true && cb2.Checked=false) then
+        textbox.Text<-"Установлена только первая галочка"
+    if (cb1.Checked=false && cb2.Checked=true) then
+        textbox.Text<-"Установлена только вторая галочка"
+    if (cb1.Checked=true && cb2.Checked=true) then
+        textbox.Text<-"Обе галочки установлены "
+   
+    window.Controls.Remove(textbox)         //отчищаем текстбокс
+    window.Controls.Add(textbox)
+    |> ignore
+
+
+let _ = button.Click.Add(knopka_press)     //триггер нажатия
 
 
 [<EntryPoint>]
 let main argv =
-    do Application.Run(form)
+    do Application.Run(window)            //запускаю форму
     0 // return an integer exit code
