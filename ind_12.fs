1b7647c1dada65d1e086fbddf28091959c88b8b3
m-cyx <markpixel05@gmai.com>
2020-06-10 11:06:40 +0300

    вывод листа рекурсивный

--- ind_12.fs
+++ ind_12.fs
@@ -11,6 +11,20 @@ let rec read_list n =
     let Tail = read_list (n-1)
     Head::Tail
 
+    
+let rec write_list (count,num) = 
+    match count,num with
+    |h1::tail,h2::tail2 when h1>0 ->
+        System.Console.Write(int(h2));
+        System.Console.Write("\n");
+        let newcount = 
+            let head = h1-1
+            head::tail
+        write_list (newcount,num)
+
+    |h1::tail,h2::tail2 -> write_list (tail,tail2)
+    |_,_ -> ()
+
     [<EntryPoint>]
 let main argv = 
     0 // return an integer exit code
